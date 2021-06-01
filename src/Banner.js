13d462972cf906f99a1f873641cd0cd54db7d10a
Eddy Chase <eddykanyz.ek@gmail.com>
2021-06-01 03:53:04 +0300

    Banner with movie date fetched on to it

--- src/Banner.js
+++ src/Banner.js
@@ -29,12 +29,12 @@ function Banner() {
     return (
         <header className='banner'
             style={{
-                backgroundSize: "cover",
-                backgroundImage: 'url("https://wholistickids.com/wp-content/uploads/2019/01/plain-black-background.jpg")',
-                backgroundPosition: "center center",
+                backgroundSize: 'cover',
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                backgroundPosition: 'center center',
             }}>
             <div className='banner_contents'>
-                <h1 className='banner_title'> Movie Title </h1>
+                <h1 className='banner_title'> {movie?.title || movie?.name || movie?.original_name} </h1>
                 <div className='banner_buttons'>
                     <button className='banner_button'>
                         play
@@ -44,10 +44,7 @@ function Banner() {
                     </button>
                 </div>
                 <h1 className='banner_description'>
-                    {Truncate(
-                        'This is a Description',
-                        150)}
-
+                    {Truncate(movie?.overview, 150)}
                 </h1>
             </div>
             <div className="banner_fadeBottom" />
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './HomeScreen.css';
 import Nav from './Nav';
 import Banner from './Banner';
+import requests from './Requests';
+import Row from './Row';
 
 function HomeScreen() {
     return (
@@ -11,7 +13,14 @@ function HomeScreen() {
             <Banner />
 
 
-            { /* Row*/}
+            < Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} iSLargeRow />
+            < Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+            < Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+            < Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+            < Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+            < Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+            < Row title='Romance Movies' fetchUrl={requests.fetchRomanticMovies} />
+            < Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
 
         </div>
     )
